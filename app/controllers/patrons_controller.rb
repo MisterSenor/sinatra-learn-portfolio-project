7bb29d44a08e49050a1b16bd7f0d08109a05b7c7
MisterSenor <nathan.visak@gmail.com>
2019-01-21 13:04:10 -0600

    refactor of Patron model class, added valid_params? method

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -5,4 +5,29 @@ class PatronsController < ApplicationController
     erb :'/patrons/index'
   end
 
+  get '/patrons/new' do
+    erb :'/patrons/new'
+  end
+
+  get '/patrons/errors/error' do
+    erb :'/patrons/errors/error'
+  end
+
+  post '/patrons' do
+    if params["work"]["artist_id"] && params["artist"]
+      erb :'/patrons/errors/too_much_artist_input'
+    elsif Patron.valid_params?(params)
+      if params["work"]["artist_id"] ####this is not finished yet
+      redirect to '/patrons'
+      end 
+    else
+      redirect to '/patrons/error'
+    end
+  end
+
+    get '/patrons/:id/edit' do
+      @patron = Patron.find_by_id(params["id"])
+      erb :'/patrons/edit'
+    end
+
 end
