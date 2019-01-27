0338ec4bdfbbf5b5b5d012a90e39f73f3ce7f21b
MisterSenor <nathan.visak@gmail.com>
2019-01-27 11:51:02 -0600

    still working on the edit action of Patron class

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -17,41 +17,65 @@ class PatronsController < ApplicationController
     erb :'/patrons/errors/too_much_artist_input'
   end
 
-  post '/patrons' do
-    if params["work"]["artist_id"] && !params["artist"].empty?
-      redirect to "/patrons/errors/too_much_artist_input"
-    end
-    if Patron.valid_params?(params)
-      if params["work"]["artist_id"]
-        #now make a patron with the pre-existing artist
-        @patron = Patron.create(name: params["patron"])
-        @work = Work.create(name: params["work"]["name"], year_completed: params["work"]["year_completed"])
-        @patron.works << @work
-        @artist = Artist.find_by_id(params["work"]["artist_id"][0])
-        @artist.works << @work
-        redirect to '/patrons'
-      elsif params["artist"]
-        #now create a new artist
-        @patron = Patron.create(name: params["patron"])
-        @work = Work.create(name: params["work"]["name"], year_completed: params["work"]["year_completed"])
-        @artist = Artist.create(name: params["artist"])
-        @artist.works << @work
-        @patron.works << @work
-        redirect to '/patrons'
-      end
+  post '/patrons/new' do
+    @patron = Patron.create(name: params["patron_name"])
+    @work = Work.create(name: params["work"]["name"], year_completed: params["work"]["year_completed"])
+    if params["work"]["artist_id"] != nil
+      @artist = Artist.find_by_id(params["work"]["artist_id"])
+      @work.artist = @artist
+      @patron.works << @work
+      @artist.works << @work
     else
-      redirect to '/patrons/error'
+      @artist = Artist.create(name: params["artist"])
+      @work.artist = @artist
+      @patron.works << @work
+      @artist.works << @work
     end
+    redirect to "/patrons"
   end
 
-    get '/patrons/:id' do
-      @patron = Patron.find_by_id(params["id"])
-      erb :'/patrons/show'
-    end
+  get '/patrons/:id' do
+    @patron = Patron.find_by_id(params["id"])
+    erb :'/patrons/show'
+  end
 
-    get '/patrons/:id/edit' do
-      @patron = Patron.find_by_id(params["id"])
-      erb :'/patrons/edit'
+  get '/patrons/:id/edit' do
+    @patron = Patron.find_by_id(params["id"])
+    erb :'/patrons/edit'
+  end
+
+  patch '/patrons/:id' do
+    @patron = Patron.find_by_id(params["id"])
+    @patron.update(name: params["patron_name"])
+  #on line 52, "if there's no new artist name and there are no checkboxes for artists checked"
+    if params["artist"].empty? && params["patron_works"]["artist_id"] == nil
+      new_work_array = []
+      params["patron_works"]["work_ids"].each do |work|
+        new_work_array << work.to_i
+      end
+      old_work_array = []
+      @patron.works.each do |work|
+        old_work_array << work
+      end
+      deleted_work = old_work_array - new_work_array
+      @work = Work.find_by_id(deleted_work)
+      @patron.works - @work
     end
+    if !params["work"]["name"].empty?#the params form with a new work name isn't empty
+      @work = Work.create(name: params["work"]["name"], year_completed: params["work"]["year_completed"])
+      if params["patron_works"].keys.include?("artist_id")
+        @artist = Artist.find_by_id(params["patron_works"]["artist_id"])
+        @work.artist = @artist
+        @artist.works << @work
+      else
+        @artist = Artist.create(name: params["artist"])
+        @work.artist = @artist
+        @artist.works << @work
+      end
+      @patron.works << @work
+    end
+    redirect to "/patrons"
+  end
+
 
 end
--- app/models/patron.rb
+++ app/models/patron.rb
@@ -2,8 +2,4 @@ class Patron < ActiveRecord::Base
   has_many :works
   has_many :artists, through: :artists_patrons
 
-  def self.valid_params?(params)
-    return !params["patron"].empty? && !params["work"]["name"].empty? && !params["work"]["year_completed"].empty? && (params["work"]["artist_id"] != nil || !params["artist"].empty?)
-  end
-
 end
