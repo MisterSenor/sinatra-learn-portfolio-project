88f4f008348aea13ace9f7b3c0c8f33d552df8cd
MisterSenor <nathan.visak@gmail.com>
2019-02-13 12:30:14 -0600

    Fixed wonky Patron data problem where Patrons could be initialized with missing attributes

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -23,20 +23,31 @@ class PatronsController < ApplicationController
 
   post '/patrons/new' do
     redirect_if_not_logged_in
+    if params["patron_name"] == "" || params["patron_name"] == nil || params["patron_name"] = /\s*/
+      redirect to '/patrons/errors/error'
+    elsif params["work"]["name"] == nil || params["work"]["name"] == "" || params["work"]["name"] == /\s*/
+      redirect to '/patrons/errors/error'
+    elsif params["work"]["year_completed"] == nil || params["work"]["year_completed"] == "" || params["work"]["year_completed"] == /\s*/
+      redirect to '/patrons/errors/error'
+      #My intention with line 32 is to make sure that there is at least a new artist initialized or an existing artist associated with this new patron object
+    elsif (params["artist"] == nil) && (!params["work"]["artist_id"])
+      redirect to '/patrons/errors/error'
+    else
     @patron = Patron.create(name: params["patron_name"])
     @work = Work.create(name: params["work"]["name"], year_completed: params["work"]["year_completed"])
-    if params["work"]["artist_id"] != nil
-      @artist = Artist.find_by_id(params["work"]["artist_id"])
-      @work.artist = @artist
-      @patron.works << @work
-      @artist.works << @work
-    else
-      @artist = Artist.create(name: params["artist"])
-      @work.artist = @artist
-      @patron.works << @work
-      @artist.works << @work
-    end
+      if params["work"]["artist_id"] != nil
+        @artist = Artist.find_by_id(params["work"]["artist_id"])
+        @work.artist = @artist
+        @patron.works << @work
+        @artist.works << @work
+      else
+        @artist = Artist.create(name: params["artist"])
+        @work.artist = @artist
+        @patron.works << @work
+        @artist.works << @work
+      end
     redirect to "/patrons"
+    end
   end
 
   get '/patrons/:id' do
@@ -56,6 +67,7 @@ class PatronsController < ApplicationController
     @patron = Patron.find_by_id(params["id"])
     @patron.update(name: params["patron_name"])
   #below this line should read: "if there's no new artist name and there are no checkboxes for artists checked"
+  #In other words, if you want to change the works that belong to a patron, but not create any new works
     if params["artist"].empty? && params["patron_works"]["artist_id"] == nil
       new_work_array = []
       params["patron_works"]["work_ids"].each do |work|
